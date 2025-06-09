d000db8c9772f6b4d2c4ac1b189cfec54cc7b113
FranNogui <116107405+FranNogui@users.noreply.github.com>
2025-06-09 16:14:10 +0200

    Primera versión de la página

---
+++ pages/2_2._🧑‍🎓_Titulados en Extremadura.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+def typeOfStudy(name):
+  firstWord = name.split(' ')[0]
+  dobleGrado = ['DOBLE', 'DOB', 'PCEO:']
+  master = ['MÁSTER']
+  if firstWord in dobleGrado: return 'DOBLE GRADO'
+  elif firstWord in master:   return 'MÁSTER'
+  else: return 'GRADO'
+
+def prepareDataFrame(df):
+  df = df.drop(['uri', 'ou_indicadoresTitulacion'], axis=1)
+  df['ID_Titulacion'] = df['rdfs_label'].apply(lambda x: x[-5:-1])
+  df['rdfs_label'] = df['rdfs_label'].apply(lambda x: x[len(' Indicadores demandaUniversitara XXXX '):-6])
+  df['Tipo_Estudio'] = df['rdfs_label'].apply(lambda x : typeOfStudy(x))
+  df['ou_cursoAcademico'] = df['ou_cursoAcademico'].apply(lambda x: x[-4:])
+  df = df.rename(columns={'ou_notaMinimaAccesoMayor45': 'NMAMayor45',
+                          'ou_hombresPrimeraMatriculaPrimerCurso': 'HPrMatPrC',
+                          'ou_extranjerosPrimeraMatricula': 'ExtrPrMat',
+                          'ou_mujeresPrimeraMatriculaPrimerCurso': 'MPrMatPrC',
+                          'ou_hombresPrimeraMatricula': 'HPrMat',
+                          'ou_mujeresPrimeraMatricula': 'MPrMat',
+                          'ou_notaMinimaAccesoTitulados': 'NMATitulados',
+                          'ou_espanolesPrimeraMatricula': 'EspPrMat',
+                          'ou_notaMinimaAccesoDeportistas': 'NMADeportistas',
+                          'ou_notaMinimaAccesoGeneral': 'NMAGeneral',
+                          'rdfs_label': 'Titulacion',
+                          'ou_notaMinimaAccesoMayor25': 'NMAMayor25',
+                          'ou_notaMinimaAccesoMayor40': 'NMAMayor40',
+                          'ou_notaMinimaAccesoDiscapacitados': 'NMADiscapacitados',
+                          'ou_cursoAcademico': 'Anyo'
+                        })
+  df = df[['Anyo', 'Titulacion', 'ID_Titulacion', 'Tipo_Estudio', 
+          'EspPrMat', 'ExtrPrMat', 'HPrMatPrC', 'MPrMatPrC', 'HPrMat', 'MPrMat',
+          'NMAGeneral', 'NMATitulados', 'NMAMayor25', 'NMAMayor40', 'NMAMayor45', 'NMADeportistas', 'NMADiscapacitados']]
+  return df
+
+@st.cache_data
+def loadDataFrame(year):
+  file_dir = f".\\Data\\Extremadura\\IndicadoresDemandaUniversitaria{year}.csv"
+  df = pd.read_csv(file_dir, encoding='utf-8')
+  return prepareDataFrame(df)
+
+def loadDataFrames(yearStart, yearEnd):
+  frames=[]
+  for i in range(yearStart, yearEnd+1):
+    frames.append(loadDataFrame(i))
+  return pd.concat(frames, ignore_index=True)
+
+def createDataFrame(gender, start_year, end_year, num_tit):
+  df = loadDataFrames(start_year, end_year)
+  columns = []
+  if gender == 'Hombre':  columns = ['HPrMat']
+  elif gender == 'Mujer': columns = ['MPrMat']
+  elif gender == 'Ambos': columns = ['HPrMat', 'MPrMat']
+  columns += ['ID_Titulacion', 'Titulacion']
+  df = df[columns].fillna(0)
+
+  if gender == 'Ambos':
+    df['Num. Titulados'] = df['HPrMat'] + df['MPrMat']
+    df = df[['Num. Titulados', 'ID_Titulacion', 'Titulacion']]
+  elif gender == 'Hombre':
+    df = df.rename(columns={'HPrMat': 'Num. Titulados'})
+  else:
+    df = df.rename(columns={'MPrMat': 'Num. Titulados'})
+
+  df = df.groupby('Titulacion', as_index=False)['Num. Titulados'].sum()
+  df = df.sort_values(by=['Num. Titulados'], ascending=False, ignore_index=True)
+
+  df = df.head(num_tit)
+
+  df = df.sort_values(by=['Num. Titulados'], ascending=True, ignore_index=True)
+
+  return df
+
+def main():
+  st.set_page_config(
+    page_title="Titulaciones Extremadura",
+    page_icon="🧑‍🎓",
+    layout='wide'
+  )
+  st.header("Titulaciones con más alumnos titulados en Extremadura")
+
+  gender = st.sidebar.radio("Género:", ["Hombre", "Mujer", "Ambos"])
+  start_year = st.sidebar.slider("Año inicio:", 2008, 2025, 2008, 1)
+  if start_year < 2025:
+    end_year = st.sidebar.slider("Año final:", start_year, 2025, step=1) 
+  else:
+    end_year = 2025
+
+  num_tit = st.sidebar.slider("Número de titulaciones:", 1, 50, step=1)
+
+  df = createDataFrame(gender, start_year, end_year, num_tit)
+  fig = px.bar(df, x='Num. Titulados', y='Titulacion', labels={'Titulacion': 'Titulación', 'Num. Titulados': 'Número de Titulados'})
+  fig.update_layout(title_text="Número de alumnos titulados por titulación", title_x=0.5, height= 80 + 920 * (num_tit / 50) ,margin=dict(l=20, r=20, t=40, b=30))
+  st.plotly_chart(fig, use_container_width=True)
+
+main()
---
+++ pages/3_3._🏫_Centros_Universitarios_en_España.py
@@ -0,0 +1,135 @@
+import streamlit as st
+import geopandas as gpd
+import pandas as pd
+import folium
+import json
+from streamlit_folium import st_folium
+from branca.colormap import linear, LinearColormap
+
+universidad_a_provincia = {
+    "Alcalá de Henares": "Madrid",
+    "Alicante": "Alacant/Alicante",
+    "Almería": "Almería",
+    "Autónoma de Barcelona": "Barcelona",
+    "Autónoma de Madrid": "Madrid",
+    "Barcelona": "Barcelona",
+    "Burgos": "Burgos",
+    "Cádiz": "Cádiz",
+    "Cantabria": "Cantabria",
+    "Carlos III": "Madrid",
+    "Castilla-La Mancha": "Ciudad Real",
+    "Complutense de Madrid": "Madrid",
+    "Córdoba": "Córdoba",
+    "Extremadura": "Badajoz",
+    "Girona": "Girona",
+    "Granada": "Granada",
+    "Huelva": "Huelva",
+    "Illes Balears": "Illes Balears",
+    "Islas Baleares": "Illes Balears",
+    "Jaén": "Jaén",
+    "Jaume I de Castellón": "Castelló/Castellón",
+    "La Rioja": "La Rioja",
+    "León": "León",
+    "Lleida": "Lleida",
+    "Málaga": "Málaga",
+    "Miguel Hernández de Elche": "Alacant/Alicante",
+    "Murcia (1)": "Murcia",
+    "Murcia": "Murcia",
+    "Oviedo": "Asturias",
+    "Pablo de Olavide": "Sevilla",
+    "País Vasco": "Bizkaia",
+    "Politécnica de Cartagena (1)": "Murcia",
+    "Politécnica de Cartagena": "Murcia",
+    "Politécnica de Catalunya": "Barcelona",
+    "Politécnica de Cataluña": "Barcelona",
+    "Politécnica de Madrid": "Madrid",
+    "Politécnica de Valencia": "València/Valencia",
+    "Católica de Valencia":"València/Valencia",
+    "Pompeu Fabra": "Barcelona",
+    "Pública de Navarra": "Navarra",
+    "Rey Juan Carlos": "Madrid",
+    "Rovira i Virgili": "Tarragona",
+    "Salamanca": "Salamanca",
+    "Santiago de Compostela (2)": "A Coruña",
+    "Santiago de Compostela": "A Coruña",
+    "Santiago": "A Coruña",
+    "Sevilla": "Sevilla",
+    "U.N.E.D.": "Madrid",
+    "Valencia (Est. General)": "València/Valencia",
+    "Valladolid": "Valladolid",
+    "Zaragoza": "Zaragoza",
+    "Abat Oliba-CEU": "Barcelona",
+    "Alfonso X El Sabio": "Madrid",
+    "Camilo José Cela": "Madrid",
+    "Católica S. Antonio de Murcia": "Murcia",
+    "Deusto": "Bizkaia",
+    "Europea de Madrid": "Madrid",
+    "Francisco de Vitoria": "Madrid",
+    "Internacional de Catalunya": "Barcelona",
+    "Internacional de La Rioja": "La Rioja",
+    "Internacional  de La Rioja": "La Rioja",
+    "Navarra": "Navarra",
+    "Oberta de Catalunya": "Barcelona",
+    "Ramón Llull": "Barcelona",
+    "San Jorge": "Zaragoza",
+    "UDIMA": "Madrid",
+    "Vic": "Barcelona",
+    "TOTAL": "TOTAL",
+    "UNIVERSIDADES PÚBLICAS": "UNIVERSIDADES PÚBLICAS",
+    "UNIVERSIDADES PRIVADAS": "UNIVERSIDADES PRIVADAS",
+    "La Laguna": "Canarias",
+    "Palmas (Las)": "Canarias",
+    "UNIVERSIDADES PUBLICAS": "UNIVERSIDADES PUBLICAS",
+    "Coruña, A": "A Coruña",
+    "Coruña, La": "A Coruña",
+    "Vigo": "A Coruña",
+    "Antonio de Nebrija": "Madrid",
+    "Cardenal Herrera-CEU": "Castelló/Castellón",
+    "Católica de Avila": "Ávila",
+    "Católica de Ávila": "Ávila",
+    "Internal. de Cataluña": "Barcelona",
+    "Mondragón": "Gipuzkoa",
+    "Pontificia Comillas": "Madrid",
+    "Pontificia de Salamanca": "Salamanca",
+    "SEK": "Madrid",
+    "San Pablo-CEU": "Madrid",
+    "Europea Miguel de Cervantes": "Valladolid",
+    "Internal. de Catalunya": "Barcelona",
+    "IE Universidad": "Madrid",
+    "Internacional Valenciana": "València/Valencia",
+    "Internacional Menéndez Pelayo": "Madrid",
+    "Internacional de Andalucía": "Sevilla",
+    "Católica S.Antonio de Murcia": "Murcia",
+    "CENTROS ADSCRITOS A UNIV. PUBLICAS": "CENTROS ADSCRITOS A UNIV. PUBLICAS"
+}
+
+@st.cache_data
+def loadData():
+  directory = '.\\Data\\Espanya\\Provincias\\recintos_provinciales_inspire_peninbal_etrs89.shp'
+  provincias = gpd.read_file(directory)
+  provincias = provincias.to_crs("EPSG:4326")
+  provincias = provincias[['NAMEUNIT', 'geometry']]
+  provincias = provincias.rename(columns={'NAMEUNIT': 'Provincia'})
+  provinciasNames = provincias.drop(columns=['geometry'])
+  gdf = gpd.GeoDataFrame(provincias)
+
+  return provincias, provinciasNames, gdf
+
+def main():
+  prov, provNames, gdf = loadData()
+
+  gdf['geometry'] = gdf['geometry'].simplify(0.001, preserve_topology=True)
+
+  m = folium.Map(zoom_start=6)
+
+  prov['geometry'] = prov['geometry'].simplify(0.001, preserve_topology=True)
+
+  bounds = gdf.total_bounds
+  m.fit_bounds([[bounds[1], bounds[0]], [bounds[3], bounds[2]]])
+
+  geojson = folium.GeoJson(data=json.loads(gdf.to_json()))
+  geojson.add_to(m)
+
+  st_folium(m, use_container_width=True, height=600, returned_objects=[])
+
+main()
